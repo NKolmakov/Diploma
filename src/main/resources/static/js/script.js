446bd0f65a34bd7ee8f5d637adc63c1b3467a9f5
Nikita_Kolmakov <colmakov.nikita@mail.ru>
2019-08-22 19:03:15 +0300

    reactive behavior is ready. All element's names is ready. Reactive operations: create and delete. Controller takes valid test.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,100 +1,126 @@
 var questNumber = 0;
-var questHeaderNumber = 1;
 var questionPlaceholder = document.getElementById("form.placeholder.question").value;
 var answerPlaceholder = document.getElementById("form.placeholder.answer").value;
 var questionNumber = document.getElementById("form.label.questionNumber").value;
 var addAnswer = document.getElementById("form.button.addAnswer").value;
 
-$(function (){
-    $(".createQuestion").click(function (){
-        var html2Add =
-        "<div id='question#"+ ++questNumber +"' class='question'>"+
-            "<input hidden id='lastAnsIndex#"+questNumber+"' value='1'>"+
-            "<h4>"+questionNumber+ ++questHeaderNumber +"</h4>"+
-            "<button class='removeQuestion' type='button'> - </button><br>"+
-            "<textarea name='questions["+questNumber+"].name' placeholder='"+questionPlaceholder+"' maxlength='255' required></textarea>"+
-            "<button class='createAnswer' type='button'>"+addAnswer+"</button>"+
-            "<div id='answers#"+questNumber+"' class='answers'>"+
-                "<div id='answer#0'>"+
-                    "<input name='questions["+questNumber+"].answers[0].right' type='checkbox'>"+
-                    "<input name='questions["+questNumber+"].answers[0].name' type='text' placeholder='"+answerPlaceholder+"' maxlength='255' required><br>"+
-                "</div>"+
-            "</div>"+
-        "</div>";
-        $(".questionBlock").prepend(html2Add);
-    })
-});
+function createQuestion() {
+    var parentObject = $(".questionBlock")[0];
+    var questions = $(parentObject).find("div[id^='question#']");
+    var questionLength = questions.length;
 
-$(function (){
-    $(".questionBlock").on("click",".createAnswer",function (){
-        var currentQuestionNumber = $(this).parent().attr("id").replace("question#","");
-        var currentAnswer = $(this).parent().find("div[id^='answers#']");
-        var lastAnswer = document.getElementById("lastAnsIndex#"+currentQuestionNumber);
-        lastIndex = lastAnswer.value;
+    if (questionLength == null) {
+        questionLength = 0;
+    }
 
-        var html2Add =
-        "<div id='answer#"+lastIndex+"'>"+
-             "<input name='questions["+questNumber+"].answers["+lastIndex+"].right' type='checkbox'>"+
-             "<input name='questions["+questNumber+"].answers["+lastIndex+"].name' type='text' placeholder='"+answerPlaceholder+"' maxlength='255' required>"+
-             "<button class='removeAnswer' type='button'> - </button><br>"+
-         "</div>";
-        $(currentAnswer).append(html2Add);
-        $(lastAnswer).val(++lastIndex);
-    })
-});
+    $(parentObject)
+        .prepend($('<div id="question#' + questionLength + '" class="question"></div>')
+            .prepend($('<h4 class="questionHeader col-lg-6">' + questionNumber + (questionLength + 1) + '</h4>'))
+            .append($('<button class="removeQuestion"> - </button>'))
+            .append($('<textarea name="questions[' + questionLength + '].name" class="questionName"' +
+                'placeholder="' + questionPlaceholder + '"' +
+                'maxlength="255" required' +
+                '</textarea>'))
+            .append($('<button class="createAnswer" type="button">' + addAnswer + '</button>'))
+            .append($('<div id="answer#0" class="answers"</div>')
+                .append($('<input name="questions[' + questionLength + '].answers[0].right" type="checkbox">'))
+                .append($('<input name="questions[' + questionLength + '].answers[0].right" type="text"' +
+                    'placeholder="' + answerPlaceholder + '" maxlength="255" required><br>'))
+            )
+        );
+}
 
-$(function () {
-    $(".questionBlock").on("click",".removeAnswer",function (){
-        var currentQuestionNumber = $(this).parent().parent().parent().attr("id").replace("question#","");
-        var lastAnswer = document.getElementById("lastAnsIndex#"+currentQuestionNumber);
-        lastIndex = lastAnswer.value;
+function removeQuestion(question) {
+    $(question).remove();
+    var questions = $($(".questionBlock")[0]).find("div[id^='question#']");
+    updateQuestions(questions);
+}
 
-        var currentAnswer = $(this).parent();
-        $(currentAnswer).remove();
-        $(lastAnswer).val(--lastIndex);
-        updateQuestions();
-    })
-});
+function updateQuestions(questions) {
+    if (questions != null) {
+        for (var i = 0; i < questions.length; i++) {
+            var currentQuestionNumber = questions.length - i - 1;
+            questions[i].id = "question#" + currentQuestionNumber;
+            var answers = $(questions[i]).find("div[id^='answer#']");
+            var questHeader = $(questions[i]).find(":header[class*='questionHeader']")[0];
+            var textarea = $(questions[i]).find("textarea[class*='questionName']")[0];
 
-$(function (){
-    $(".questionBlock").on("click",".removeQuestion",function(){
-        var currentQuestion = $(this).parent();
-        $(currentQuestion).remove();
-
-        var parentObject = document.getElementsByClassName("questionBlock")[0];
-        var nestedObjects = parentObject.getElementsByTagName("H4");
-
-        for(var i=0; i < nestedObjects.length; i++){
-            nestedObjects[i].innerHTML = questionNumber+(nestedObjects.length-i);
+            questHeader.innerHTML = questionNumber+parseInt(currentQuestionNumber+1,10);
+            textarea.setAttribute('name','questions['+currentQuestionNumber+'].name')
+            updateAnswers(answers);
+        }
+    }
+}
+    function createAnswer(question) {
+        var questNumber = $(question).attr('id').replace("question#", "");
+        var answers = $(question).find("div[id^='answer#']");
+        var answerLength = answers.length;
+        if (answerLength == null) {
+            answerLength = 0;
         }
 
-        questHeaderNumber = nestedObjects.length;
-        updateQuestions();
-    })
-});
+        var firstAnswer = $(question).find("div[id^='answer#']")[0];
+        $(firstAnswer)
+            .before($('<div id="answer#' + answerLength + '" class="answers"</div>')
+                .append($('<input name="questions[' + questNumber + '].answers[' + answerLength + '].right" type="checkbox">'))
+                .append($('<input name="questions[' + questNumber + '].answers[' + answerLength + '].name" type="text"' +
+                    'placeholder="' + answerPlaceholder + '" maxlength="255" required>'))
+                .append($('<button class="removeAnswer" type="button"> - </button><br>'))
+            )
+    }
 
-function updateQuestions(){
-    var parentObject = document.getElementsByClassName("questionBlock")[0];
-    var questions = $(parentObject).find("div[id^='question#']");
+    function removeAnswer(answer) {
+        var currentQuestion = $(answer).closest(".question")[0];
+        $(answer).remove();
 
-    for(var i=0; i<questions.length; i++){
-        questions[i].id = "question#"+(questions.length-i-1);
-        var answers = $(questions[i]).find("div[id^='answers#']");
-        for(var j = 0; j < answers.length; j++){
-            answers[j].id = "answers#"+(questions.length-i-1);
-            var childAnswers = $(answers[j]).find("div[id^='answer#']");
+        var currentQuestionNumber = $(currentQuestion).attr('id').replace("question#", "");
+        var answers = $(currentQuestion).find("div[id^='answer#']");
 
-            for(var k = 0; k < childAnswers.length; k++){
-                childAnswers[k].id = "answer#"+k;
-                var inputCheckbox = $(childAnswers[k]).find("input:checkbox");
-                var inputText = $(childAnswers[k]).find("input:text");
+        updateAnswers(answers);
+    }
 
-                inputCheckbox.name = "questions["+i+"].answers["+k+"].right";
-                inputText.name = "questions["+i+"].answers["+k+"].name";
+    function updateAnswers(answers) {
+        if (answers.length != null) {
+            for (var i = 0; i < answers.length; i++) {
+                var answerNum = answers.length - i - 1;
+                var currentQuestionNumber = $($(answers[i]).closest(".question")[0]).attr('id').replace("question#","");
+                var inputCheckBox = $(answers[i]).find("input:checkbox")[0];
+                var inputText = $(answers[i]).find("input:text")[0];
+                var newInputText = $(inputText).clone()[0];
+                var newCheckbox = $(inputCheckBox).clone()[0];
+                answers[i].id = "answer#" + answerNum;
+
+                newCheckbox.setAttribute('name','questions[' + currentQuestionNumber + '].answers[' + answerNum + '].right');
+                newCheckbox.setAttribute('value',$(inputCheckBox).val());
+                newInputText.setAttribute('name', 'questions[' + currentQuestionNumber + '].answers[' + answerNum + '].name');
+                newInputText.setAttribute('value', $(inputText).val());
+
+                inputCheckBox.parentNode.replaceChild(newCheckbox, inputCheckBox);
+                inputText.parentNode.replaceChild(newInputText, inputText);
             }
         }
     }
 
-    questNumber = questions.length-1;
-}
+    $(function () {
+        $(".createQuestion").click(function () {
+            createQuestion();
+        })
+    });
 
+    $(function () {
+        $(".questionBlock").on("click", ".createAnswer", function () {
+            createAnswer($(this).closest(".question")[0]);
+        })
+    });
+
+    $(function () {
+        $(".questionBlock").on("click", ".removeAnswer", function () {
+            removeAnswer($(this).closest(".answers")[0]);
+        })
+    });
+
+    $(function(){
+        $(".questionBlock").on("click",".removeQuestion",function(){
+            removeQuestion($(this).closest(".question")[0]);
+        })
+    });
